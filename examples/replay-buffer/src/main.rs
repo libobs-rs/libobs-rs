06354a43b71ffbda14844f187b4f7e3f7aa0a22a
Hendrik Lind <hendrik@sshcrack.me>
2026-03-29 01:12:01 +0100

    fix: replay buffers on linux (#166)
    * fix: add symlinks to target directory
    
    * fix: create symlinks to obs binaries to make replay buffer work on linux
    
    * update readme

--- examples/replay-buffer/src/main.rs
+++ examples/replay-buffer/src/main.rs
@@ -1,14 +1,15 @@
 #[cfg(windows)]
 mod windows;
 
+#[cfg(target_os = "linux")]
+mod linux;
+
 #[cfg(windows)]
 fn main() {
     windows::main().unwrap();
 }
 
-#[cfg(not(windows))]
+#[cfg(target_os = "linux")]
 fn main() {
-    println!(
-        "This example only supports windows at the moment, but you can implement similar code on Linux."
-    );
+    linux::main().unwrap();
 }
